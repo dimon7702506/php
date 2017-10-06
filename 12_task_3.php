0acd419df7430472f94dcaf84d1d7e5164a60789
dimon7702506 <7702506@gmail.com>
2017-10-06 16:32:01 +0300

    '20171006_1'

--- 12_task_3.php
+++ 12_task_3.php
@@ -3,47 +3,28 @@
 
 function delete_dir($dir)
 {
+    if(!file_exists($dir)){
+        echo "Директория не существует" . "<br>";
+        return null;
+    }
     $files = array_diff(scandir($dir), array('.', '..'));
+
+    //var_dump($files);
+
     foreach ($files as $file) {
         (is_dir("$dir/$file")) ? delete_dir("$dir/$file") : unlink("$dir/$file");
     }
     return rmdir($dir);
 }
 
-function delete_dir1($path)
-{
-    if (!file_exists($path)){
-        return false;
-    }
-    $files = scandir($path);
-
-    foreach ($files as $file){
-
-        if ($file === '.' || $file === '..'){
-            continue;
-        }
-
-        $file_path = $path . DIRECTORY_SEPARATOR . $file;
-
-        if (is_dir($file_path)) {
-            delete_dir($file_path);
-        }else{
-            echo $file_path;
-            unlink($file_path);
-        }
-
-        if (rmdir($path)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+if (PHP_OS === "Linux") {
+    $dir = '/var/www/homework.com/public_html/tmp/tmp';
+} else {
+    $dir = 'D:\OSPanel\domains\source\homework\tmp\tmp';
 }
 
-//if (PHP_OS === "Linux") {
-    $dir = '/var/www/homework.com/public_html/tmp/tmp123';
-//} else {
-//    $dir = '';
-//}
-
-delete_dir($dir);
+if (delete_dir($dir)) {
+    echo "Директория успешно удалена";
+}else{
+    echo "Директория не удалена";
+}
