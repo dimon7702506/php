2d5f0b1a68f44f1c9a7d80d69d2416a3d4b84e21
dimon <7702506@gmail.com>
2017-10-10 17:39:22 +0300

    20171010_1

--- 12_send.php
+++ 12_send.php
@@ -18,24 +18,21 @@ function write_to_file($user_name, $message):void
 function read_messages($param_microtime):array
 {
     $file = fopen(CHAT_FILE_NAME, 'r');
-    $mes = [
-        'time' => '',
-        'user' => '',
-        'message' => ''
-    ];
-
+    $mes = [];
+    $row = [];
     while ($row = fgetcsv($file)) {
-        $row = fgetcsv($file);
-
-        $mes['time'] = $row[0];
-        $mes['user'] = $row[1];
-        $mes['message'] = $row[2];
-        //var_dump($row);
+        array_push($mes, $row);
     }
     fclose($file);
-    var_dump($mes);
-    echo count($mes);
 
+    $mes = array_reverse($mes);
+
+    if($param_microtime){
+        echo "no";
+    }else{
+        $mes = array_slice($mes, 0, 50);
+    }
+    //var_dump($mes);
     return $mes;
 }
 
@@ -44,5 +41,31 @@ if ($_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {
     $msg = $_POST['Message'] ?? '';
     $param_microtime = $_POST['param_microtime'] ?? '';
     write_to_file($user, $msg);
-    read_messages($param_microtime);
+    $arrays = read_messages($param_microtime);
+
+    $response = [
+        'success' => true,
+        'param_time' => '',
+        'messages' => [],
+    ];
+
+    $messages = [];
+    foreach ($arrays as $array) {
+        if ($temp_param_microtime == 0) {
+            $temp_param_microtime = $array[0];
+        }
+        $mes = [
+            'user' => $array[1],
+            'message' => $array[2],
+            'time' => date('d-m-y H:i:s', (float)$array[0])
+            ];
+        array_push($messages, $mes);
+    }
+    $response['param_time'] = $temp_param_microtime;
+    $response['messages'] = $messages;
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+
+
 }
